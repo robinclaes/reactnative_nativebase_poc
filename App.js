257cd8a8a2324c80a2f44b504f8977363e0d4751
Robin Claes <robin.claes@icapps.com>
2017-11-30 19:33:53 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,114 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  Item,
+  Form,
+  Input,
+  Footer,
+  FooterTab,
+  Button,
+  Left,
+  Right,
+  Body,
+  Icon,
+  Text
+} from 'native-base';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name="menu" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Header</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content>
+          <Form>
+            <Item>
+              <Input placeholder="Username" />
+            </Item>
+            <Item>
+              <Input placeholder="Username" />
+            </Item>
+            <Item>
+              <Input placeholder="Username" />
+            </Item>
+            <Item>
+              <Input placeholder="Username" />
+            </Item>
+            <Item>
+              <Input placeholder="Username" />
+            </Item>
+            <Item last>
+              <Input placeholder="Password" />
+            </Item>
+            <Item last>
+              <Input placeholder="Password" />
+            </Item>
+            <Item last>
+              <Input placeholder="Password" />
+            </Item>
+            <Item last>
+              <Input placeholder="Password" />
+            </Item>
+            <Item last>
+              <Input placeholder="Password" />
+            </Item>
+            <Item last>
+              <Input placeholder="Password" />
+            </Item>
+            <Item last>
+              <Input placeholder="Password" />
+            </Item>
+            <Item last>
+              <Input placeholder="Password" />
+            </Item>
+          </Form>
+        </Content>
+        <Footer>
+          <FooterTab>
+            <Button full>
+              <Text>Footer</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5
+  }
+});
